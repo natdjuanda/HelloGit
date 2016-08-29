f8d77cd0734ef050b5143e4e068e859ddadb8b04
Nathan <nathandjuanda@gmail.com>
2016-08-29 22:34:03 +1000

    yoooo

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -6,7 +6,7 @@ namespace HelloGit
     {
         public static void Main (string [] args)
         {
-            Console.WriteLine ("Hello Dog!");
+            Console.WriteLine ("Yo!");
             Console.Read ();
         }
     }
