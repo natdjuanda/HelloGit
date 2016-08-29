cd9e2910a208e31ab83f1558ffdcdccf8eced7d6
Nathan <nathandjuanda@gmail.com>
2016-08-29 22:14:34 +1000

    try to greet my dog

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -6,7 +6,8 @@ namespace HelloGit
     {
         public static void Main (string [] args)
         {
-            Console.WriteLine ("Hello World!");
+            Console.WriteLine ("Hello Dog!");
+            Console.Read ();
         }
     }
 }
