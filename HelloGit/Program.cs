97f57a6a72aed450834308fb8f5d4ad279cfe21e
Nathan <nathandjuanda@gmail.com>
2016-09-01 22:54:42 +1000

    added white space

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -6,7 +6,10 @@ namespace HelloGit
     {
         public static void Main (string [] args)
         {
-            
+            Console.WriteLine ("This is test number 3");
+            Console.Read ();
+
+
         }
     }
 }
