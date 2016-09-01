ccb421afddbaa0e5bed290a9464ab4f924d09cd4
Nathan <nathandjuanda@gmail.com>
2016-09-01 22:39:14 +1000

    deleted the main procedure

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -6,8 +6,7 @@ namespace HelloGit
     {
         public static void Main (string [] args)
         {
-            Console.WriteLine ("Yo!");
-            Console.Read ();
+            
         }
     }
 }
